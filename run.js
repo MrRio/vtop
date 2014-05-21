2848a7d67c495129fd7fe249b4ea8a7a7264de07
James Hall <james@snapshotmedia.co.uk>
2014-05-21 05:15:19 +0100

    Add memory usage

--- run.js
+++ run.js
@@ -4,10 +4,12 @@ var line = require('bresenham');
 var size = require('window-size');
 
 var width = Math.floor(size.width / 2) * 4;
-var height = Math.floor((size.height) / 16) * 24;
+var height = Math.floor((size.height) / 16) * 36;
 
 
 var c = new Canvas(width, height);
+var m = new Canvas(width, height);
+
 
 var i = 0;
 
@@ -30,14 +32,17 @@ setInterval(function() {
   });
 }, 100);
 
+var currentMemUsage = 0;
+setInterval(function() {
+  currentMemUsage = 100 - Math.floor(os.freememPercentage() * 100);
+}, 100);
+
 var position = 0;
 var cpuPositions = [];
+var memPositions = [];
 
 function draw() {
   c.clear();
-  c.set(1, 1);
-  c.set(1, 2);
-  c.set(1, 3);
 
   for (y = 1; y < height; y ++) {
     c.set(1, y);
@@ -49,6 +54,18 @@ function draw() {
     c.set(x, height - 1);
   }
 
+  m.clear();
+
+  for (y = 1; y < height; y ++) {
+    m.set(1, y);
+    m.set(width - 1, y);
+  }
+
+  for (x = 1; x < width; x ++) {
+    m.set(x, 1);
+    m.set(x, height - 1);
+  }
+
   position = position + 1;
 
   setTimeout(draw, 100);
@@ -58,10 +75,7 @@ function draw() {
   }
   console.log('Parastats v0.0.1');
 
-  drawHeader('Network', '10%');
-  console.log(c.frame());
-
-  cpuPositions[position] = height - Math.floor((height / 100) * currentCpuUsage) - 2;
+  cpuPositions[position] = height - Math.floor((height / 100) * currentCpuUsage) - 1;
 
   for (var pos in cpuPositions) {
     var p2 = parseInt(pos) + (width - cpuPositions.length);
@@ -74,8 +88,18 @@ function draw() {
   drawHeader('CPU', currentCpuUsage + '%');
   console.log(c.frame());
 
-  drawHeader('Memory', '6%');
-  console.log(c.frame());
+  memPositions[position] = height - Math.floor((height / 100) * currentMemUsage) - 1;
+
+  for (var pos in memPositions) {
+    var p2 = parseInt(pos) + (width - cpuPositions.length);
+    if (p2 < 1) {
+      continue;
+    }
+    m.set(p2, memPositions[pos]);
+  }
+
+  drawHeader('Memory', currentMemUsage + '%');
+  console.log(m.frame());
 
 }
 
