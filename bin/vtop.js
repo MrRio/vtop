283f2b68ec39e1e198a3f529a71268b702064b69
James Hall <james@snapshotmedia.co.uk>
2014-06-14 12:09:20 +0100

    Fix for people that have set xterm-color in their dotfiles on Mac #2

--- bin/vtop.js
+++ bin/vtop.js
@@ -1,4 +1,6 @@
 #!/bin/sh
+':' //; # This line below fixes xterm color bug on Mac - https://github.com/MrRio/vtop/issues/2
+':' //; if [[ $TERM == "xterm-color" && "$OSTYPE" == "darwin"* ]]; then export TERM=xterm-256color; fi
 ':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
 
 require('../app.js');
