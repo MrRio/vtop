ca8c47f7e004367f8d834ccc674eff8ab0d196cb
David Davidović <geosoft.corp@gmail.com>
2015-01-15 09:54:27 +0100

    Spaces to tabs

--- sensors/memory.js
+++ sensors/memory.js
@@ -18,40 +18,41 @@ var plugin = {
 	 * @type {String}
 	 */
 	type: 'chart',
-	/**
-	 * The default interval time in ms that this plugin should be polled.
-	 * More costly benchmarks should be polled less frequently.
-	 */
+		/**
+		 * The default interval time in ms that this plugin should be polled.
+		 * More costly benchmarks should be polled less frequently.
+		 */
 	interval: 200,
 
 	initialized: false,
 
 	currentValue: 0,
 
-    isLinux: _os.platform().indexOf('linux') != -1,
+	isLinux: _os.platform().indexOf('linux') != -1,
 
-	/**
-	 * Grab the current value, from 0-100
-	 */
+		/**
+		 * Grab the current value, from 0-100
+		 */
 	poll: function() {
-        var computeUsage = function(used, total) {
-            return Math.round(100 * (used / total));
-        };
+		var computeUsage = function(used, total) {
+			return Math.round(100 * (used / total));
+		};
 
-        if (plugin.isLinux) {
-            child.exec('free -m', function(err, stdout, stderr) {
-                var data = stdout.split('\n')[1].replace(/[\s\n\r]+/g, ' ').split(' ');
-                
-                var used = parseInt(data[2]);
-                var total = parseInt(data[1]);
-                plugin.currentValue = computeUsage(used, total);
-            });
-        } else {
-            plugin.currentValue = Math.round((1 - os.freememPercentage()) * 100);
-        }
+		if (plugin.isLinux) {
+			child.exec('free -m', function(err, stdout, stderr) {
+				var data = stdout.split('\n')[1].replace(/[\s\n\r]+/g, ' ').split(' ');
+
+				var used = parseInt(data[2]);
+				var total = parseInt(data[1]);
+				plugin.currentValue = computeUsage(used, total);
+			});
+		} else {
+			plugin.currentValue = Math.round((1 - os.freememPercentage()) * 100);
+		}
 
 		plugin.initialized = true;
 	}
 };
+
 module.exports = exports = plugin;
 
