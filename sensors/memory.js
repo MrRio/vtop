131fbd7fdde34f470c1e91f45d7a40ce10b024b5
James Hall <james@parall.ax>
2016-10-16 19:45:59 +0100

    Convert source to StandardJS

--- sensors/memory.js
+++ sensors/memory.js
@@ -4,55 +4,57 @@
  * (c) 2014 James Hall
  */
 
-var os = require('os-utils');
-var _os = require('os');
-var child = require('child_process');
+var os = require('os-utils')
+var _os = require('os')
+var child = require('child_process')
 
 var plugin = {
-	/**
-	 * This appears in the title of the graph
-	 */
-	title: 'Memory Usage',
-	/**
-	 * The type of sensor
-	 * @type {String}
-	 */
-	type: 'chart',
-	/**
-	 * The default interval time in ms that this plugin should be polled.
-	 * More costly benchmarks should be polled less frequently.
-	 */
-	interval: 200,
+  /**
+   * This appears in the title of the graph
+   */
+  title: 'Memory Usage',
+  /**
+   * The type of sensor
+   * @type {String}
+   */
+  type: 'chart',
+  /**
+   * The default interval time in ms that this plugin should be polled.
+   * More costly benchmarks should be polled less frequently.
+   */
+  interval: 200,
 
-	initialized: false,
+  initialized: false,
 
-	currentValue: 0,
+  currentValue: 0,
 
-	isLinux: _os.platform().indexOf('linux') != -1,
+  isLinux: _os.platform().indexOf('linux') !== -1,
 
-	/**
-	 * Grab the current value, from 0-100
-	 */
-	poll: function() {
-		var computeUsage = function(used, total) {
-			return Math.round(100 * (used / total));
-		};
+  /**
+   * Grab the current value, from 0-100
+   */
+  poll: function () {
+    var computeUsage = function (used, total) {
+      return Math.round(100 * (used / total))
+    }
 
-		if (plugin.isLinux) {
-			child.exec('free -m', function(err, stdout, stderr) {
-				var data = stdout.split('\n')[1].replace(/[\s\n\r]+/g, ' ').split(' ');
+    if (plugin.isLinux) {
+      child.exec('free -m', function (err, stdout, stderr) {
+        if (err) {
+          console.error(err)
+        }
+        var data = stdout.split('\n')[1].replace(/[\s\n\r]+/g, ' ').split(' ')
 
-				var used = parseInt(data[2]);
-				var total = parseInt(data[1]);
-				plugin.currentValue = computeUsage(used, total);
-			});
-		} else {
-			plugin.currentValue = Math.round((1 - os.freememPercentage()) * 100);
-		}
+        var used = parseInt(data[2])
+        var total = parseInt(data[1])
+        plugin.currentValue = computeUsage(used, total)
+      })
+    } else {
+      plugin.currentValue = Math.round((1 - os.freememPercentage()) * 100)
+    }
 
-		plugin.initialized = true;
-	}
-};
+    plugin.initialized = true
+  }
+}
 
-module.exports = exports = plugin;
-
+module.exports = exports = plugin
